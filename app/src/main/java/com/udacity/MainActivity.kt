253aa48e6a5f3d2993c7164148b5d866b2d17dbf
Babatunde Owoleke <tundeowoleke1@gmail.com>
2022-01-17 18:18:28 +0100

    Implement the feature to download the selected file.

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -9,6 +9,7 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -32,34 +33,95 @@ class MainActivity : AppCompatActivity() {
 
         custom_button.setOnClickListener {
             custom_button.buttonState = ButtonState.Clicked
-            download()
+
+            val downloadURL = when (downloadGroup.checkedRadioButtonId) {
+                glide_download.id -> GLIDE_URL
+                loadApp_download.id -> LOAD_APP_URL
+                retrofit_download.id -> RETROFIT_URL
+                else -> null
+            }
+            download(downloadURL)
         }
+
+
     }
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            if (downloadID == id) {
+                val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+                val query = DownloadManager.Query()
+                query.setFilterById(id)
+                val cursor = downloadManager.query(query)
+
+                if (cursor.moveToFirst()) {
+
+                    val status = when (cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_STATUS))) {
+                        DownloadManager.STATUS_SUCCESSFUL -> "Success"
+                        DownloadManager.STATUS_FAILED -> "Error"
+                        else -> "Unknown"
+                    }
+
+                    val fileName = cursor.getString(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_TITLE))
+
+                    val fileDescription = cursor.getString(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_DESCRIPTION))
+
+
+
+                }
+            }
+
+
         }
+
+
     }
 
-    private fun download() {
-        val request =
-            DownloadManager.Request(Uri.parse(URL))
-                .setTitle(getString(R.string.app_name))
-                .setDescription(getString(R.string.app_description))
-                .setRequiresCharging(false)
-                .setAllowedOverMetered(true)
-                .setAllowedOverRoaming(true)
+    private fun download(downloadURL: String?) {
+        if (downloadURL != null) {
+            val request =
+                DownloadManager.Request(Uri.parse(downloadURL))
+                    .setRequiresCharging(false)
+                    .setAllowedOverMetered(true)
+                    .setAllowedOverRoaming(true)
+                    .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
 
-        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-        downloadID =
-            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+            when (downloadURL) {
+                LOAD_APP_URL -> {
+                    request.setTitle(getString(R.string.app_name))
+                    request.setDescription(getString(R.string.app_description))
+                }
+
+                GLIDE_URL -> {
+                    request.setTitle(getString(R.string.glide))
+                    request.setDescription(getString(R.string.glide_description))
+                }
+
+                RETROFIT_URL -> {
+                    request.setTitle(getString(R.string.retrofit))
+                    request.setDescription(getString(R.string.retrofit_description))
+                }
+            }
+
+            val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+            downloadID =
+                downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+        } else {
+            Toast.makeText(applicationContext, getString(R.string.no_download_selection), Toast.LENGTH_SHORT).show()
+        }
+
     }
+
+
 
     companion object {
-        private const val URL =
+        private const val GLIDE_URL = "https://github.com/bumptech/glide/archive/refs/heads/master.zip"
+
+        private const val LOAD_APP_URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
-        private const val CHANNEL_ID = "channelId"
+
+        private const val RETROFIT_URL = "https://github.com/square/retrofit/archive/refs/heads/master.zip"
     }
 
 }
